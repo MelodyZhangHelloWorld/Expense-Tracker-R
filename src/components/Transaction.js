dcf1eed08286020212023465a87f8ffaf1415935
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2020-12-08 21:26:42 -0700

    transactions1
    Signed-off-by: MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export const Transaction = () => {
+export const Transaction = ({transaction}) => {
+    const sign = transaction.amount > 0? '+':'-';
+    let plusMinus = transaction.amount>0?'plus':'minus';
+
     return (
         <div>
-            
+            <li className={plusMinus}>
+                      {transaction.text} 
+                      <span> {sign}${Math.abs(transaction.amount)} </span> 
+                      <button className="delete-btn">X</button> 
+                  </li>
         </div>
     )
 }
