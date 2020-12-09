787dc44ac33e098432ae1f03c1797b075accd40a
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2020-12-08 22:55:51 -0700

    changedBacktoBT'sBetterWay
    Signed-off-by: MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,8 +4,8 @@ import {GlobalContext} from '../context/GlobalState';
 export const Balance = () => {
     const {transactions} = useContext(GlobalContext);  //*
     const amounts = transactions.map(transaction => transaction.amount);
-    const total = amounts.reduce((acc, item)=> (acc+item)).toFixed(2) //*
-                //amounts.reduce((acc, item)=> (acc += item), 0).toFixed(2);
+    const total = amounts.reduce((acc, item)=> (acc+item), 0).toFixed(2) //*
+              
     return (
         <div>
             <h4>Your Balance</h4>
